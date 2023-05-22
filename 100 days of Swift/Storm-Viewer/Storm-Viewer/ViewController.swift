46aa2b8d0562b9d0f005aba8a41b0f653e2c6cee
Felipe Augusto <augusto_felipe@icloud.com>
2023-05-22 18:11:50 -0300

    no message

--- 100 days of Swift/Storm-Viewer/Storm-Viewer/ViewController.swift
+++ 100 days of Swift/Storm-Viewer/Storm-Viewer/ViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var pictures: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,11 +25,30 @@ class ViewController: UIViewController {
         
         // Passamos por todos os arquivos dentro da nossa array items
         for item in items {
-            if item.hasPrefix("nsll"){
-                // carregar os items que começam com nssl
-                item
+            if item.hasPrefix("nssl"){
+                // carregar os items que começam com nssl e coloca-los dentro da nossa array.
+                pictures.append(item)
             }
         }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
 
